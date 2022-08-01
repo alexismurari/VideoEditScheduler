2cc869fc2da2ccc7b74aad66ca8d86fdacdf1bb7
Alexis <alexismurari@hotmail.com>
2022-07-31 21:42:36 -0400

    Added Autoscheduling. App Quota limits to 6-7 video a day.

--- publish.py
+++ publish.py
@@ -1,3 +1,4 @@
+from operator import mod
 import os
 from os import listdir
 from os.path import isfile, join
@@ -12,7 +13,36 @@ from googleapiclient.http import MediaFileUpload
 
 scopes = ["https://www.googleapis.com/auth/youtube.upload"]
 
-def schedule_video(path, video):
+def schedule_video(path, video, month, day, hour, youtube):
+    request = youtube.videos().insert(
+        part="snippet,status",
+
+        body=dict(
+            snippet=dict(
+                title=video + " #shorts",
+                description="Best Gaming Clips!",
+                tags="csgo, valorant, gaming, clips, memes, gameplay, gamer, pc, youtubegaming channel",
+                categoryId="20"
+            ),
+            status=dict(
+                privacyStatus="private",
+                selfDeclaredMadeForKids = "false",
+                publishAt="2022-" + month + "-" + day + "T" + str(hour) + ":0:00.000+00:00"
+                #publishAt="2022-11-01T8:0:00.000+00:00"
+            )
+        ),
+        media_body=MediaFileUpload(path + video)
+    )
+    response = request.execute()
+
+    print(response)
+
+
+def main(args):
+    new_videos_path = args.src
+    new_videos = [f for f in listdir(new_videos_path) if isfile(join(new_videos_path, f))]
+    print(new_videos)
+
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
@@ -28,36 +58,22 @@ def schedule_video(path, video):
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, credentials=credentials)
 
-    request = youtube.videos().insert(
-        part="snippet,status",
 
-        body=dict(
-            snippet=dict(
-                title=video,
-                description="",
-                tags="csgo",
-                categoryId="20"
-            ),
-            status=dict(
-                privacyStatus="private",
-                publishAt="2022-11-01T8:20:00.000+00:00"
-            )
-        ),
-        media_body=MediaFileUpload(path + video)
-    )
-    response = request.execute()
+    month = "08"
+    day = "01"
+    hour = 0
 
-    print(response)
+    while int(day) < 31:
+        for vid in new_videos:
+            schedule_video(new_videos_path, vid, month, day, hour, youtube)
+            hour += 1
+            if hour == 24:
+                day = str(int(day) + 1)
+                if day <= str(9): day = "0" + day
+            hour = mod(hour, 24)
 
+            print(month, day, hour)
 
-def main(args):
-    new_videos_path = "edit/"
-    new_videos = [f for f in listdir(new_videos_path) if isfile(join(new_videos_path, f))]
-    print(new_videos)
-
-    for vid in new_videos:
-        print(args)
-        #schedule_video(new_videos_path, vid)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -66,6 +82,4 @@ if __name__ == "__main__":
     parser.add_argument("dest", help="Destination location")
     args = parser.parse_args()
 
-    print(args)
-
-    #main(args)
+    main(args)
