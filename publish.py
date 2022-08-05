13f3cc3ee44a1816d2e640017bd390666bd36f64
Alexis <alexismurari@hotmail.com>
2022-08-04 23:18:29 -0400

    Added method to save/refresh access token to the scheduling procedure

--- publish.py
+++ publish.py
@@ -2,13 +2,14 @@ from operator import mod
 import os
 from os import listdir
 from os.path import isfile, join
-import sys
 import argparse
+import pickle
 
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
+from google.auth.transport.requests import Request
 from googleapiclient.http import MediaFileUpload
 
 scopes = ["https://www.googleapis.com/auth/youtube.upload"]
@@ -19,9 +20,9 @@ def schedule_video(path, video, month, day, hour, youtube):
 
         body=dict(
             snippet=dict(
-                title=video + " #shorts",
+                title=video[:-4] + " #shorts",
                 description="Best Gaming Clips! #shorts #shortsvideo #shortsfeed #shortsyoutube #comedy #news",
-                tags="csgo, valorant, gaming, clips, memes, gameplay, gamer, pc, youtubegaming channel",
+                tags="csgo, valorant, gaming, clips, memes, gameplay, gamer, pc, youtubegaming channel, overwatch, apex, legends, apex legends",
                 categoryId="20"
             ),
             status=dict(
@@ -38,35 +39,83 @@ def schedule_video(path, video, month, day, hour, youtube):
     print(response)
 
 
+def get_creds(credentials):
+    client_secrets_file = "client_secret.json"
+
+    if os.path.exists('token.pickle'):
+        print("Yes")
+        with open('token.pickle', 'rb') as token:
+            print("Yes again")
+            credentials = pickle.load(token)
+    
+    print(credentials)
+
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+
+        flow.run_local_server(port=8080, prompt="consent", authorization_prompt_message="")
+
+        credentials = flow.credentials
+
+        with open('token.pickle', 'wb') as f:
+            pickle.dump(credentials, f)
+
+    print(credentials)
+
+    return credentials
+
+    # # Disable OAuthlib's HTTPS verification when running locally.
+    # # *DO NOT* leave this option enabled in production.
+    # os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    # api_service_name = "youtube"
+    # api_version = "v3"
+    
+    # # Get credentials and create an API client
+    # flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+    #     client_secrets_file, scopes)
+    # credentials = flow.run_console()
+    # youtube = googleapiclient.discovery.build(
+    #     api_service_name, api_version, credentials=credentials)
+
+    # flow.run_local_server(port=8080, prompt="consent", authorization_prompt_message="")
+
+    # creds = flow.credentials
+
+    # print(creds.to_json)
+    
+
+
+
+
+
 def schedule(args):
+    credentials = None
+
     new_videos_path = args.src
     new_videos = [f for f in listdir(new_videos_path) if isfile(join(new_videos_path, f))]
     print(new_videos)
 
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    credentials = get_creds(credentials)
+
 
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = "client_secret.json"
-
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
     youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
-
+    api_service_name, api_version, credentials=credentials)
 
     month = "08"
-    day = "02"
-    hour = 12
+    day = "05"
+    hour = 6
 
     while int(day) < 31:
         for vid in new_videos:
             schedule_video(new_videos_path, vid, month, day, hour, youtube)
-            hour += 4
+            hour += 6
             if hour == 24:
                 day = str(int(day) + 1)
                 if day <= str(9): day = "0" + day
