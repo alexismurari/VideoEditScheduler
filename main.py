cad938735ef3d2d90bb71f0a3b63ed111032f041
Alexis <alexismurari@hotmail.com>
2022-08-09 17:49:14 -0400

    Imported qt_material stylesheets module

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 
+# For the stylesheet templates
+# Copyright (c) 2020, GCPDS
+# All rights reserved.
+# https://github.com/UN-GCPDS/qt-material
+from qt_material import apply_stylesheet
+
 from ui.MainWindow import MainWindow
 
 import sys
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    apply_stylesheet(app, theme='dark_teal.xml')
     window = MainWindow()
     window.show()
     app.exec()
